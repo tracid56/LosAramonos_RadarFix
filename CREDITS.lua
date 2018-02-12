b6a680d9d717d4b73b1ac0ad3b5c7f1169f4c61d
Walter White <walterwhiteheisenberg84@outlook.fr>
2018-02-12 18:12:34 +0100

    Mise a jour

--- CREDITS.lua
+++ CREDITS.lua
@@ -7,3 +7,9 @@
 #         2018         #
 #                      #
 ########################
+
+--[
+
+Si vous utilisez le script ou une partie du script pour quelconque code merci d'utiliser le credit ci dessus 
+
+--]
